2eb1410d62f222ffd16ce611a4ae18ffb94cb3f4
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-03-20 18:38:00 -0300

    Primer problema resuelto

--- Lab0v2/A/pizza.cpp
+++ Lab0v2/A/pizza.cpp
@@ -1,10 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <math.h>
+
 using namespace std;
 
 int main(){
-    int nfriends,h;
-    cin>>nfriends;
-    cout<<nfriends;
+    double rp,dp;
+    int n;
+    cin>>rp>>dp>>n;
+    double x[n];
+    double y[n];
+    double r[n];
 
+    for(int i=0;i<n;i++){
+        cin>>x[i]>>y[i]>>r[i];
+    }
+
+    double total = 0;
+    double distancia;
+
+    for(int i=0;i<n;i++){
+        distancia = sqrt(x[i]*x[i] + y[i]*y[i]);
+
+        if(distancia >= r[i] + (rp-dp) && distancia + r[i] <= rp){ // Fuera circulo interno, dentro de la pizza
+            total++;
+        }
+    }
+
+    cout<<total;
     return 0;
 }
