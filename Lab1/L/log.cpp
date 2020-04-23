59e00119af0372c860860add78d0d0c7e1a8aed6
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-23 19:09:34 -0400

    Avance Cristian/Max

--- Lab1/L/log.cpp
+++ Lab1/L/log.cpp
@@ -1,17 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef float ull;
+typedef double ull;
 
+// Compara si fact >= exp sin calcular fact completamente
+bool factorial_comp(ull n, ull exp)
+{
+    ull r = 0;
+    for (ull i = n; i >= 1; i--) // De mayores a menores
+    {
+        r += log(i) / n;
+        
+        if (r >= exp){
+            return true;
+        }
+    }
+    return r >= exp;
+}
+
+// log(n!) = SUM log(i->n)
+// Dividimos por N, porque para el otro lado ya no tenemos N*log(a), sino log(a)
 ull factorial(ull n)
 {
     ull r = 0;
     for (ull i = 1; i <= n; i++)
     {
-        r += log2(i);
+        r += log(i) / n;
+        //cout<<r<<"\n";
     }
     return r;
 }
 
+// Aproxx stirling
 ull factorial2(ull n)
 {
     ull r = 0;
@@ -19,9 +38,10 @@ ull factorial2(ull n)
     return r;
 }
 
+// N*log(a), pero el N pasa dividiendo al factorial
 ull exponencial(ull a, ull n)
 {
-    ull r = n * log2(a);
+    ull r = log(a);
     return r;
 }
 
@@ -44,18 +64,19 @@ int main(){
     for(int z = 0; z < t; z++) 
     {
         A = 1;
-        B = a[z]*4;
+        B = 2718274 + 1;
 
         while (A != B){
             mid = A + (B - A)/2;
-            exp = exponencial(a[z], mid);
-            fact = factorial2(mid);
-            cout<<"A: "<<A<<" B: "<<B<<"\n";
-            cout<<"MID: "<<mid<<"\n";
-            cout<<"EXP : "<<exp<<"\n";
-            cout<<"FACT: "<<fact<<"\n";
+            // exp = exponencial(a[z], mid); // Exp es sólo calcular log(a) => + rápido que fact
+            exp = log(a[z]);
+            // fact = factorial(mid);
+            // cout<<"A: "<<A<<" B: "<<B<<"\n";
+            // cout<<"MID: "<<mid<<"\n";
+            // cout<<"EXP : "<<exp<<"\n";
+            // cout<<"FACT: "<<fact<<"\n";
 
-            if( fact >= exp )
+            if( factorial_comp(mid, exp) )
             {
                 B = mid; // Bajamos
             }
@@ -65,20 +86,20 @@ int main(){
             }
         }
 
-        fact = factorial2(A);
-        exp = exponencial(a[z], A);
-        cout<<"EXP FINAL : "<<exp<<"\n";
-        cout<<"FACT FINAL: "<<fact<<"\n";
+        // fact = factorial(A);
+        // exp = exponencial(a[z], A);
+        // // cout<<"EXP FINAL : "<<exp<<"\n";
+        // // cout<<"FACT FINAL: "<<fact<<"\n";
         
-        if( exp > fact ) // No hay un n entre A y B que cumpla la condicion
-        {
-            cout<<"ERROR\n";
-            return 1;
-        }
-        else
-        {
+        // if( exp > fact ) // No hay un n entre A y B que cumpla la condicion
+        // {
+        //     cout<<"ERROR\n";
+        //     return 1;
+        // }
+        // else
+        // {
             a[z] = A; // A cumple
-        }
+        // }
     }
 
     //cout<<"\n";
