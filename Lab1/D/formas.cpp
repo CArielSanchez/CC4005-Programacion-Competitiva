f20a91f1407067b5cf163656d40153f093219710
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-26 00:40:12 -0400

    Implementado el algoritmo del viernes

--- Lab1/D/formas.cpp
+++ Lab1/D/formas.cpp
@@ -5,40 +5,58 @@ using namespace std;
 int main(void)
 {
     int n;
-    int total = 0;
-    int resp=0;
+    int formas=0;
 
     cin>>n;
-    int a[n];
+    vector<int> a(n);
+    vector<int> sumaAcum(n);
+    vector<int> dosKIndices;
     for(int i=0; i < n ;i++){
         cin >> a[i];
-        total += a[i];
+        if(i == 0){
+            sumaAcum[0] = a[0];
+        }else{
+            //cout<<"a[i]: "<<a[i]<<" sumaAcum{i-1]: "<<sumaAcum[i-1]<<"\n";
+            sumaAcum[i] = a[i] + sumaAcum[i-1];
+        }
     }
-    
-    
-    
-    if (total % 3 == 0)
+
+    // for (size_t i = 0; i < n; i++)
+    // {
+    //     cout<<sumaAcum[i]<<" ";
+    // }
+    // cout<<"aa\n";
+
+    if (sumaAcum[n-1] % 3 == 0)
     {
-        int valor_g = total/3;
-        int formas = 0;
-        int sum = 0;
-    
+        int saco = sumaAcum[n-1] / 3;
 
-        for(int i=0;i<n-1;i++)
+        for (size_t i = 0; i < n; i++)
         {
-            sum += a[i];
-        if ( sum  == valor_g * 2) // Encontré el segundo conjunto: el que me da dos veces la suma dividida en 3, entonces esta la puedo permutar con las formas que ya encontré
-            {
-                resp += formas; // Entonces sumo la cantidad de formas que llevo porque el conjunto que encontré lo puedo permutar con todos los que llevo
-                
-        }
-        
-        if (sum == valor_g) // Encontré el primer conjunto: el que me da la primera suma
-            formas++;
+            if(sumaAcum[i] == 2*saco && i != n-1){
+                dosKIndices.push_back(i);
+            }
         }
 
-            
-            
+        // for (size_t i = 0; i < dosKIndices.size(); i++)
+        // {
+        //     cout<<dosKIndices[i]<<" ";
+        // }
+        // cout<<"\n";
+        
+
+        for (size_t i = 0; i < n; i++)
+        {
+            if(sumaAcum[i] == saco){
+                for (size_t j = 0; j < dosKIndices.size(); j++)
+                {
+                    if( dosKIndices[j] > i){
+                        formas++;
+                    }
+                }
+                
+            }
+        }
     }
-    cout<<resp;
+    cout<<formas;
 }
