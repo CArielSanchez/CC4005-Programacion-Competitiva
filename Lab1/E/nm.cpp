93f47466e860b749106dfbfa16b9b3d51ca7cc25
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-26 16:15:26 -0400

    Fix detalle del E, y funciona!

--- Lab1/E/nm.cpp
+++ Lab1/E/nm.cpp
@@ -1,43 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(){
-    long long n, m, k = 0;
+    ll n, m, k = 0;
 
     cin>>n>>m>>k;
 
-    long long A = 1;
-    long long B = n * m;
-    long long medio = 0;
-    long long menoresAMi = 0;
+    ll A = 1;
+    ll B = n * m;
+    ll medio;
+    ll menoresAMi;
+    ll anterior = 0;
     while(A < B) { // Buscando resultado
         medio = A + (B - A)/2;
         menoresAMi = 0;
-        //cout<<"Medio: "<<medio<<"\n";
+        //cout<<"A: "<<A<<" B: "<<B<<" Medio: "<<medio<<"\n";
 
-        for (long long i = 1; i <= n; i++)
+        for (ll i = 1; i <= n; i++)
         {
-            //cout<<"Menores a mi: "<<min(m, medio/i)<<"\n";
+            //cout<<"Menores/iguales a mi: "<<min(m, medio/i)<<"\n";
             menoresAMi += min(m, medio/i); // En la i-esima fila, el peor caso es que toda la fila es menor al resultado, el mejor que solo mid/i sea menor (Si h*i > medio => h > medio/i)
         }
-        //cout<<"Total Menores a mi: "<<menoresAMi<<"\n";
+        //cout<<"Total Menores/iguales a mi: "<<menoresAMi<<"\n";
         
         if (menoresAMi < k)
         {
             A = medio + 1;
-        }else if (menoresAMi > k)
+        }
+        else // Buscamos con uno más chico
         {
             B = medio;
-        }else{
-            break;
+            anterior = medio;
         }
     }
 
-    if(A == B){ // Líimite derecho
-        medio = A;
+    if(A < k && anterior > k){ // cuando busco para abajo pero me paso por harto por que hay por ej {1 {2} {2} 4}
+        A = anterior;
     }
     
-    cout<<medio;
+    cout<<A;
 
     return 0;
 }
