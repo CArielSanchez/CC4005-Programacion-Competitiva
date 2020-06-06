5f84f66df50d016af66ec8826ae068e65ec01ab3
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-06-05 20:29:46 -0400

    a

--- Lab3/A/a.cpp
+++ Lab3/A/a.cpp
@@ -2,18 +2,40 @@
 using namespace std;
 typedef long long ll;
 
-ll mod = pow(10, 9) + 7;
+ll mod = 1e9 + 7;
 
-ll gcd(ll a,ll b) {
-    ll R;
-    while ((a % b) > 0)  {
-        //cout<<"gcd("<<a<<","<<b<<")\n";
-        R = (a % b);
-        a = b;
-        b = R;
-    }
-    return b;
+// ll gcd(ll a,ll b) {
+//     ll R;
+//     while ((a % b) > 0)  {
+//         //cout<<"gcd("<<a<<","<<b<<")\n";
+//         R = (a % b);
+//         a = b;
+//         b = R;
+//     }
+//     return b;
+// }
+
+ll fastPow(ll x, ll n){
+    //cout<<"fastpow("<<x<<","<<n<<")\n";
+    if (n == 0) return 1LL;
+    ll mid = fastPow(x, n / 2) % mod;
+    //cout<<"mid: "<<mid<<"\n";
+    ll val = (mid * mid) ;
+    if (n % 2 == 1) val = (val * x);
+    return val;
 }
+
+ll findMax(ll array[], ll n){
+    ll max = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+    return max;
+}
+
 
 int main(){
     ll n;
@@ -29,18 +51,29 @@ int main(){
         sum += nums[i];
     }
     //cout<<"sum: "<<sum<<"\n";
-    ll t = (ll) pow(x, sum);
+    ll t = fastPow(x, sum);
+    //cout<<t<<"\n";
     
-    ll s = 0;
+    // for (ll i = 0; i < n; i++)
+    // {
+    //     s += (ll) fastPow(x, sum - nums[i]) % mod;
+    // }
+    ll max = findMax(nums, n);
+    ll fact = fastPow(x, sum-max);
+    ll fact2 = 0;
     for (ll i = 0; i < n; i++)
     {
-        s += (ll) pow(x, sum - nums[i]);
+        fact2 += fastPow(x, max-nums[i]);
+        fact2 = fact2 % mod;
     }
+
+    ll s = (fact * fact2);
+    
     
     //cout<<s<<"\n";
     //cout<<t<<"\n";
 
-    ll r = gcd(t, s);
+    ll r = __gcd(t, s);
     cout<<r % mod;
     return 0;
 }
