ddbebfed3fb22e340f2d2284545026aac972dcd0
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-06-18 23:25:24 -0400

    print mas rapido

--- Lab3/B/b.cpp
+++ Lab3/B/b.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+//typedef long long int;
 
-ll N_primos = 10000001;
+int N_primos = 10000001;
 
-ll primos[10000001];
-ll F[10000001]; // F[i] = # elementos en X, done i divide a X_j
-ll S[10000001]; // Suma acumulada de F
+int primos[10000001];
+int F[10000001]; // F[i] = # elementos en X, done i divide a X_j
+int S[10000001]; // Suma acumulada de F
 
 // Update primos[]
 void criba(){
     primos[0] = 0;
     primos[1] = 0;
-    for(ll i=2; i< N_primos; i++){
+    for(int i=2; i< N_primos; i++){
         primos[i] = -1; // -1 === primo
     }
-    for(ll i=2; i< N_primos; i++){
-        for(ll j=2; i*j < N_primos; j++){
+    for(int i=2; i< N_primos; i++){
+        for(int j=2; i*j < N_primos; j++){
             primos[i*j] = i; // Pongo primo más grande que lo divide
         }
     }
 }
 
 // Factorizacion prima de numero X
-vector<ll> fact_prima(ll X){
-    vector<ll> fact;
+vector<int> fact_prima(int X){
+    vector<int> fact;
 
     while(primos[X] != -1){
         fact.push_back(primos[X]);
@@ -36,40 +36,42 @@ vector<ll> fact_prima(ll X){
 
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     criba();
 
     // Input
-    ll n;
+    int n;
     cin>>n;
-    ll x[n];
-    for(ll i = 0; i<n; i++){
+    int x[n];
+    for(int i = 0; i<n; i++){
         cin>>x[i];
     }
 
-    ll m;
+    int m;
     cin>>m;
-    ll li[m];
-    ll ri[m];
-    for(ll i = 0; i<m; i++){
+    int li[m];
+    int ri[m];
+    for(int i = 0; i<m; i++){
         cin>>li[i]>>ri[i];
     }
 
     // Calc fact. prima de cada x.
-    vector<vector<ll>> fp_x;
-    for(ll i = 0; i<n; i++){
-        vector<ll> prr = fact_prima(x[i]);
+    vector<vector<int>> fp_x;
+    for(int i = 0; i<n; i++){
+        vector<int> prr = fact_prima(x[i]);
         fp_x.push_back(prr);
     }
 
     // Precomputar F[i]
-    for(ll i = 0; i< 10000001; i++){
+    for(int i = 0; i< 10000001; i++){
         if(primos[i] != -1){ // Si no es primo, no cuenta para [l, r]
             F[i] = 0;
         }else{               // Es primo
             F[i] = 0;
-            for(ll j = 0; j< n; j++){ // Itero sobre todos los X
+            for(int j = 0; j< n; j++){ // Itero sobre todos los X
 
-                for(ll k = 0; k < fp_x[j].size(); k++){  // Recorro su fact prima
+                for(int k = 0; k < fp_x[j].size(); k++){  // Recorro su fact prima
 
                     if(fp_x[j][k] == i){ // Si aparesco en la factorización prima de X_k, sumo y salgo
                         F[i]++;
@@ -82,11 +84,11 @@ int main(){
 
     // Calcular S === F acumulado
     S[0] = F[0];
-    for(ll i = 1; i< 10000001; i++){
+    for(int i = 1; i< 10000001; i++){
         S[i] = S[i-1] + F[i];
     }
 
-    for(ll i = 0; i<m; i++){
+    for(int i = 0; i<m; i++){
         cout<<S[ri[i]] - S[li[i]-1]<<"\n";
     }
 
